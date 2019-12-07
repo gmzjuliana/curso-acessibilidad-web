37a838f2650e7cbf07b2f9b2565fdd7a44ef0ed4
Juliana Gomez <juliana@gomezjuliana.com>
2019-12-07 18:17:59 -0500

    add styles and clean up

--- index.js
+++ index.js
@@ -5,35 +5,38 @@ window.onload = () => {
     .querySelector(".send-button")
     .addEventListener("click", showNotification);
   document.querySelectorAll(".project").forEach(element => {
-    element.addEventListener("click", (e) => openModal(e));
+    element.addEventListener("click", e => openModal(e));
   });
-  document.body.addEventListener('click', (e) => closeModal(e))
+  document.body.addEventListener("click", e => closeModal(e));
 };
 
+/** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
 function clickRight() {
   const currentLeft = parseInt(
     getComputedStyle(document.querySelector(".project-container")).left,
     10
   );
-  if (currentLeft < -166) {
+  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
     return;
   }
-  let newValue = currentLeft - 166;
+  let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
 }
 
+/** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
 function clickLeft() {
   const currentLeft = parseInt(
     getComputedStyle(document.querySelector(".project-container")).left,
     10
   );
-  if (currentLeft === 0) {
+  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
     return;
   }
-  let newValue = currentLeft + 166;
+  let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
 }
 
+/** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
   document.querySelector(".notification").style.display = "flex";
   setTimeout(function() {
@@ -41,15 +44,20 @@ function showNotification() {
   }, 3000);
 }
 
+/** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
-    document
-    .querySelector(".modal-container").style.display = "flex";
+  document.querySelector(".modal-container").style.display = "flex";
 }
 
+/** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
-    if (e.target.className.includes("project") || e.target.className === "modal") {
-        return
-    } else {
-        document.querySelector(".modal-container").style.display = "none";
-    }
+  // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
+  if (
+    e.target.className.includes("project") ||
+    e.target.className === "modal"
+  ) {
+    return;
+  } else {
+    document.querySelector(".modal-container").style.display = "none";
+  }
 }
