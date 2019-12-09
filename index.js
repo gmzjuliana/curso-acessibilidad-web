1e19577d6d5a7fe3fd7af7754838d02ee71d32a0
Juliana Gomez <juliana@gomezjuliana.com>
2019-12-08 19:32:25 -0500

    validar el formulario

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", e => showNotification(e));
+    .addEventListener("click", e => validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
@@ -89,9 +89,21 @@ function clickLeft() {
   }
 }
 
-/** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
-function showNotification(e) {
+function validateForm(e) {
   e.preventDefault();
+  const nameField = document.getElementById("name");
+  if (nameField.value === "") {
+    document.getElementById("name-error").innerHTML =
+      "! Para enviar el form, se necesita un nombre";
+  } else {
+    showNotification();
+  }
+}
+
+/** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
+function showNotification() {
+  document.getElementById("name-error").innerHTML = "";
+  document.querySelector(".form-container").reset();
   document.querySelector(".notification").style.display = "flex";
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
