07dbf5accfbc62e8c416ba9eb76139d7e833d916
Juliana Gomez <juliana@gomezjuliana.com>
2019-12-17 16:51:58 -0500

    add focus, fix styles and add esc

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ window.onload = () => {
     element.addEventListener("click", e => openModal(e));
   });
   document.body.addEventListener("click", e => closeModal(e));
+  document.body.addEventListener("keyup", e => listenForEsc(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -76,9 +77,17 @@ function showNotification() {
   }, 3000);
 }
 
+/** Escucha por la clave esc para cerrar el modal */
+function listenForEsc(e) {
+  if (e.keyCode === 27) {
+    closeModal(e);
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
+  document.getElementById('modal-header').focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
