5553d8dd64963839076b7c1ce7892169403374cc
Juliana Gomez <juliana@gomezjuliana.com>
2019-12-17 17:07:24 -0500

    validate form

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", e => validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
@@ -69,8 +69,22 @@ function clickLeft() {
   }
 }
 
+/** Validate the form before showing the notification */
+function validateForm(e) {
+  e.preventDefault();
+  const nameField = document.getElementById("name");
+  if (nameField.value === "") {
+    document.getElementById("name-error").innerHTML =
+      "! Para enviar el formulario, se necesita un nombre";
+  } else {
+    showNotification();
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById("name-error").innerHTML = "";
+  document.querySelector(".form-container").reset();
   document.querySelector(".notification").style.display = "flex";
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
