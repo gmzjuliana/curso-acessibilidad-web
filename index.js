aa326fc3fe9fda4241c5ea817994b70197c771c8
Juliana Gomez <juliana@gomezjuliana.com>
2019-12-18 08:56:12 -0500

    agregar tabindex a los botones

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project1').setAttribute("tabindex", "-1");
+      document.querySelector('.project4').removeAttribute("tabindex");
+      break;
+    case -540:
+      document.querySelector('.project2').setAttribute("tabindex", "-1");
+      document.querySelector('.project5').removeAttribute("tabindex");
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -34,6 +46,18 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project5').setAttribute("tabindex", "-1");
+      document.querySelector('.project2').removeAttribute("tabindex");
+      break;
+    case 0:
+      document.querySelector('.project4').setAttribute("tabindex", "-1");
+      document.querySelector('.project1').removeAttribute("tabindex");
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
